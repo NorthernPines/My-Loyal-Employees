ba1f16987564dc0faecfffcba7487e66e6c62fcc
Northern Pines <traviswalborn8@gmail.com>
2022-08-14 23:09:44 -0400

    working flow of inputs

--- index.js
+++ index.js
@@ -31,6 +31,85 @@ inquirer
         },
     ])
     .then((data) => {
-        const manager = new Manager(data.name, data.id, data.email, data.office);
-        team.push(manager);
+        // const manager = new Manager(data.name, data.id, data.email, data.office);
+        // team.push(manager);
+        inputNext(data.choice);
     })
+
+    function inputNext(choice) {
+        if (choice === 'Create Engineer') {
+        // for creating engineer
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: 'Please enter the engineer\'s name',
+                        name: 'name',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter the engineers\'s ID',
+                        name: 'id',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter the engineers\'s email',
+                        name: 'email',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter the engineers\'s GitHub',
+                        name: 'github',
+                    },
+                    {
+                        type: 'list',
+                        message: 'Do you have another employee to enter?',
+                        name: 'choice',
+                        choices: ['Create Engineer', 'Create Intern', 'no more employees']
+                    },
+                ])
+                .then((data) => {
+                    // const engineer = new Engineer(data.name, data.id, data.email, data.github);
+                    // team.push(engineer);
+                    inputNext(data.choice);
+                 })
+        }   else if (choice === 'Create Intern') {
+            // for creating an intern
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: 'Please enter the interns\'s name',
+                        name: 'name',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter the interns\'s ID',
+                        name: 'id',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter the interns\'s email',
+                        name: 'email',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter the interns\'s school',
+                        name: 'school',
+                    },
+                    {
+                        type: 'list',
+                        message: 'Do you have another employee to enter?',
+                        name: 'choice',
+                        choices: ['Create Engineer', 'Create Intern', 'no more employees']
+                    },
+                ])
+                .then((data) => {
+                    // const intern = new Intern(data.name, data.id, data.email, data.school);
+                    // team.push(intern);
+                    inputNext(data.choice);
+                })
+        } else {
+            // once done inputting names
+        }
+    }
