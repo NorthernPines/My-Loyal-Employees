3de46a98ca0c5cddaf9da496bf215bdfcc05f5d7
Northern Pines <traviswalborn8@gmail.com>
2022-08-15 22:58:57 -0400

    styling to webpage

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ let html =
     <title>My-Loyal-Employees</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
-  <body class="d-flex flex-wrap mx-5 px-5">
+  <body>
+  <div class="bg-light p-5 rounded-lg">
+    <h1 class="display-4 text-center">My Loyal Employees</h1>
+    <p class="lead text-center">This is a simple page to display my employees and what you need to know about them</p>
+  </div>
+  <section class="d-flex flex-wrap mx-5 px-5 justify-content-evenly">
   `
 
 console.log("\n  Manager\n-----------")
@@ -43,7 +48,7 @@ inquirer
             type: 'list',
             message: 'Do you have another employee to enter?',
             name: 'choice',
-            choices: ['Create Engineer', 'Create Intern', 'no more employees']
+            choices: ['Create Engineer', 'Create Intern', 'No more employees']
         },
     ])
     .then((data) => {
@@ -83,7 +88,7 @@ inquirer
                         type: 'list',
                         message: 'Do you have another employee to enter?',
                         name: 'choice',
-                        choices: ['Create Engineer', 'Create Intern', 'no more employees']
+                        choices: ['Create Engineer', 'Create Intern', 'No more employees']
                     },
                 ])
                 .then((data) => {
@@ -121,7 +126,7 @@ inquirer
                         type: 'list',
                         message: 'Do you have another employee to enter?',
                         name: 'choice',
-                        choices: ['Create Engineer', 'Create Intern', 'no more employees']
+                        choices: ['Create Engineer', 'Create Intern', 'No more employees']
                     },
                 ])
                 .then((data) => {
@@ -135,6 +140,7 @@ inquirer
             console.log(team);
             html += 
 `
+</section>
 </body>
 </html>`
 
